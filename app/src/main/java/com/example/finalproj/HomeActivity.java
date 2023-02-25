00dfc6901e79ddd50fb30f57772b8491a13ee695
Tom Wragg <tomwragg13@gmail.com>
2023-02-25 00:41:13 +0000

    Fixed User Info Display

--- app/src/main/java/com/example/finalproj/HomeActivity.java
+++ app/src/main/java/com/example/finalproj/HomeActivity.java
@@ -6,16 +6,48 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
 public class HomeActivity extends AppCompatActivity {
+
+    TextView nameTag;
+    TextView weightTag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        String storedEmail = null;
+        try {
+            FileInputStream inputStream = openFileInput("Email.txt"); // replace "filename.txt" with the name of the file where you stored the word
+            int length = inputStream.available();
+            byte[] buffer = new byte[length];
+            inputStream.read(buffer);
+            storedEmail = new String(buffer);
+            inputStream.close();
+
+            // do something with the word, for example, log it
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        MainActivity.fetchUserName(storedEmail);
+
+        nameTag = (TextView) findViewById(R.id.nameTag);
+        weightTag = (TextView) findViewById(R.id.weightTag);
+
+        String name = MainActivity.getUser().getName();
+        int weight = MainActivity.getUser().getWeight();
+        nameTag.setText(name);
+        weightTag.setText(weight+ " Kg");
     }
 
     @Override
