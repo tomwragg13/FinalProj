ffce985a0d6cfedef1a2619c43fed7dc0ca07cc3
Tom Wragg <tomwragg13@gmail.com>
2023-03-09 00:24:21 +0000

    Created date management system

--- app/src/main/java/com/example/finalproj/WorkoutActivity.java
+++ app/src/main/java/com/example/finalproj/WorkoutActivity.java
@@ -5,15 +5,19 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.FileInputStream;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 public class WorkoutActivity extends AppCompatActivity {
 
@@ -24,6 +28,9 @@ public class WorkoutActivity extends AppCompatActivity {
     String email;
     String name;
     int weight;
+    SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyy");
+    String date;
+    TextView dateText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +45,11 @@ public class WorkoutActivity extends AppCompatActivity {
         enterReps = (EditText) findViewById(R.id.workoutReps);
         enterWeight = (EditText) findViewById(R.id.workoutWeight);
         enterSets = (EditText) findViewById(R.id.workoutSets);
+        dateText = (TextView) findViewById(R.id.dateID);
 
+
+        date = sdf.format(new Date());
+        getDate("null");
         workouts = new ArrayList<Workouts>();
         workouts.add(new Workouts("Bench Press", 8, 45, 4, weight));
         workouts.add(new Workouts("Bicep Curl", 8, 10, 4, weight));
@@ -63,6 +74,107 @@ public class WorkoutActivity extends AppCompatActivity {
         overridePendingTransition(0,R.anim.slide_out_right);
     }
 
+    public String getDate(String direction){
+
+        int day = Integer.parseInt(date.substring(0,2));
+        int month = Integer.parseInt(date.substring(2,4));
+        int year = Integer.parseInt(date.substring(4,8));
+
+        boolean addMonth = false;
+
+        if(Objects.equals(direction, "next")){
+            day += 1;
+            if(addMonth == false){
+                if(month == 1 || month == 3 ||month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
+                    addMonth = true;
+                    if(day == 32){
+                        month+= 1;
+                        day = 1;
+                    }
+                }
+            }
+            if(addMonth == false){
+                if(month == 4 || month == 6 || month == 9 || month == 11){
+                    addMonth = true;
+                    if(day == 31){
+                        month +=1;
+                        day = 1;
+                    }
+                }
+            }
+            if(addMonth == false){
+                if(month == 2){
+                    addMonth = true;
+                    if(day == 29){
+                        month +=1;
+                        day = 1;
+                    }
+                }
+            }
+
+            if(month == 13){
+                year +=1;
+                day = 1;
+                month = 1;
+            }
+        }
+        if(Objects.equals(direction, "back")){
+            day -= 1;
+            if(addMonth == false){
+                if(month == 1 || month == 2 || month == 4 || month == 6 || month == 8 || month == 9 || month == 11){
+                    addMonth = true;
+                    if(day == 0){
+                        month -= 1;
+                        day = 31;
+                    }
+                }
+            }
+            if(addMonth == false){
+                if(month == 5 || month == 7 || month == 10 || month == 12){
+                    addMonth = true;
+                    if(day == 0){
+                        month -= 1;
+                        day = 30;
+                    }
+                }
+            }
+            if(addMonth == false){
+                addMonth = true;
+                if(month == 3){
+                    if(day == 0){
+                        month -= 1;
+                        day = 28;
+                    }
+                }
+            }
+
+            if(month == 0){
+                year -=1;
+                day = 31;
+                month = 12;
+            }
+        }
+
+
+        String dayS = String.valueOf(day);
+        String monthS = String.valueOf(month);
+        if(String.valueOf(day).length() == 1){
+            dayS = String.valueOf(0) + dayS;
+        }
+        if(String.valueOf(month).length() == 1){
+            monthS = String.valueOf(0) + monthS;
+        }
+
+        date = dayS + monthS + String.valueOf(year);
+        dateText.setText(dayS + "/" + monthS + "/" + String.valueOf(year));
+
+        Log.d("date", String.valueOf(day) + String.valueOf(month) + String.valueOf(year));
+
+
+
+        return null;
+    }
+
     public void addWorkout(View view){
         workouts.add(0,new Workouts(enterName.getText().toString(), Integer.parseInt(enterReps.getText().toString()), Integer.parseInt(enterWeight.getText().toString()), Integer.parseInt(enterSets.getText().toString()), weight));
         adapter.notifyItemInserted(0);
@@ -71,5 +183,17 @@ public class WorkoutActivity extends AppCompatActivity {
 
         adapter = new MyAdapter(getApplicationContext(), workouts);
         recyclerView.setAdapter(adapter);
+
+
+        //ClientHandler client = new ClientHandler("userData tom5 tom6 " + savedEmail + " pass 10");
+        //client.getReturnMessage();
+    }
+
+    public void nextButton(View view){
+        getDate( "next");
+    }
+
+    public void backButton(View view){
+        getDate( "back");
     }
 }
