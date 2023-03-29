a86b71e06801cb0762525251d104afa124588695
Tom Wragg <tomwragg13@gmail.com>
2023-03-30 00:10:54 +0100

    Macros statistics info complete

--- app/src/main/java/com/example/finalproj/MacrosActivity.java
+++ app/src/main/java/com/example/finalproj/MacrosActivity.java
@@ -8,16 +8,25 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -46,7 +55,17 @@ public class MacrosActivity extends AppCompatActivity {
 
     ImageView expandButton2;
 
-    ConstraintLayout recyclerHeader;
+    ConstraintLayout recyclerHeader, clickBlocker2;
+
+    float mealRecyclerHeight;
+
+    TextView nameTag;
+    TextView weightTag;
+
+    float calorieGoal = 1800f;
+    float proteinGoal = 100f;
+
+    private PieChart pieChart, pieChart2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,12 +83,25 @@ public class MacrosActivity extends AppCompatActivity {
         macrosWeight = (EditText) findViewById(R.id.macrosWeight);
         macrosWeight2 = (EditText) findViewById(R.id.macrosWeight2);
         expandButton2 = (ImageView) findViewById(R.id.expandButton2);
+        clickBlocker2 = (ConstraintLayout) findViewById(R.id.clickBlocker2);
+
+        pieChart = findViewById(R.id.pieChart);
+        pieChart2 = findViewById(R.id.pieChart2);
+
+        nameTag = (TextView) findViewById(R.id.nameTag);
+        weightTag = (TextView) findViewById(R.id.weightTag);
+
+        nameTag.setText(name);
+        weightTag.setText(weight+ " Kg");
 
 
         foodSpinner = (Spinner) findViewById(R.id.spinner);
         recyclerHeader = (ConstraintLayout) findViewById(R.id.recyclerHeader);
 
         expandButton2.setElevation(1000);
+        //recyclerHeader.setElevation(998);
+        mealRecycler.setElevation(999);
+        clickBlocker2.setElevation(980);
 
         SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyy");
         date = sdf.format(new Date());
@@ -82,20 +114,13 @@ public class MacrosActivity extends AppCompatActivity {
         foodRead();
         addIfEmpty();
 
-        //foodProduct pringles = new foodProduct("Pringles", 154, 1.7, 30);
-        //productManager Pringles = new productManager("Pringles", pringles, 30, date);
-        //productManager Pringles2 = new productManager("Pringles", pringles, 600, date);
+        mealRecycler.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                mealRecyclerHeight = mealRecycler.getY();
 
-        //food.add(Pringles);
-
-        //Log.d("Pringles", String.valueOf(Pringles.getTotalCalories()));
-        //Log.d("Pringles", String.valueOf(Pringles2.getTotalCalories()));
-
-        //mealRecycler.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-
-        //addIfEmpty();
-        //adapter = new mealAdapter(getApplicationContext(), food);
-        //mealRecycler.setAdapter(adapter);
+            }
+        });
 
 
     }
@@ -118,6 +143,8 @@ public class MacrosActivity extends AppCompatActivity {
                 mealRecycler.animate().translationYBy(px).setDuration(500);
                 expandButton2.animate().translationYBy(px).setDuration(500);
                 expandButton2.animate().rotation(90);
+
+                clickBlocker2.animate().y(0);
                 expandState = true;
             }else {
                 px = pxFromDp(getApplicationContext(), (75*food.size()-75));
@@ -125,6 +152,8 @@ public class MacrosActivity extends AppCompatActivity {
                 mealRecycler.animate().translationYBy(px).setDuration(500);
                 expandButton2.animate().translationYBy(px).setDuration(500);
                 expandButton2.animate().rotation(-90);
+
+                clickBlocker2.animate().y(mealRecyclerHeight+pxFromDp(getApplicationContext(), 75));
                 expandState = false;
             }
         }
@@ -171,15 +200,154 @@ public class MacrosActivity extends AppCompatActivity {
 
 
         Log.d("size", String.valueOf(food.size()));
-        if(food.size() == 0){
+        if(food.size() <= 1){
             expandButton2.setVisibility(View.GONE);
         }else{
             expandButton2.setVisibility(View.VISIBLE);
         }
-        //addIfEmpty();
+
+        loadPieChart();
+        loadPieChart2();
 
 
 
+    }
+
+    private void loadPieChart2() {
+        pieChart2.setDrawHoleEnabled(true);
+        pieChart2.setUsePercentValues(true);
+        pieChart2.setEntryLabelTextSize(12);
+        pieChart2.setEntryLabelColor(Color.rgb(11, 4, 82));
+        pieChart2.setCenterText("Calories");
+        pieChart2.setCenterTextSize(16);
+        pieChart2.getDescription().setEnabled(false);
+
+        ArrayList<PieEntry> entries = new ArrayList<>();
+        //entries.add(new PieEntry(0.3f, "Pie"));
+        //entries.add(new PieEntry(0.7f, ""));
+
+        double totalProtein = 0;
+        float totalPercentage = 0;
+        for(productManager product: food)    {
+            totalProtein += product.getTotalProtein();
+
+            float percentage = (float) (product.getTotalProtein()/proteinGoal);
+            totalPercentage += percentage;
+            String name = product.getName();
+            entries.add(new PieEntry(percentage, name));
+        }
+        Log.d("totalp", String.valueOf(totalPercentage));
+        if(totalPercentage < 1f){
+            entries.add(new PieEntry(1f-totalPercentage, ""));
+        }
+        pieChart2.setCenterText(String.format("%.0f", totalPercentage*100)+ "%");
+
+        ArrayList<Integer> colors = new ArrayList<>();
+        float prot = (float) totalProtein;
+        Log.d("Cals", String.valueOf(prot));
+        float percentageEaten = (prot/calorieGoal)*100;
+        if(percentageEaten <= 50){
+            for (int i = 0; i < food.size(); i++) {
+                colors.add(Color.RED);
+            }
+        }
+        if(percentageEaten > 50 && percentageEaten <= 80){
+            for (int i = 0; i < food.size(); i++) {
+                colors.add(Color.rgb(255, 106, 0));
+            }
+        }
+        if(percentageEaten > 80){
+            for (int i = 0; i < food.size(); i++) {
+                colors.add(Color.GREEN);
+            }
+        }
+
+        colors.add(Color.rgb(238, 234, 249));
+
+
+        PieDataSet dataSet = new PieDataSet(entries, "Categories");
+        dataSet.setColors(colors);
+
+        PieData data = new PieData(dataSet);
+        data.setDrawValues(true);
+        data.setValueFormatter(new PercentFormatter(pieChart2));
+        data.setValueTextSize(12f);
+        data.setValueTextColor(Color.rgb(11, 4, 82));
+
+        Legend L = pieChart2.getLegend();
+        L.setEnabled(false);
+
+        pieChart2.setData(data);
+        pieChart2.invalidate();
+    }
+
+    private void loadPieChart() {
+        //pieChart.getWidth()
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setUsePercentValues(true);
+        pieChart.setEntryLabelTextSize(12);
+        pieChart.setEntryLabelColor(Color.rgb(11, 4, 82));
+        pieChart.setCenterText("Calories");
+        pieChart.setCenterTextSize(16);
+        pieChart.getDescription().setEnabled(false);
+
+        ArrayList<PieEntry> entries = new ArrayList<>();
+        //entries.add(new PieEntry(0.3f, "Pie"));
+        //entries.add(new PieEntry(0.7f, ""));
+
+        double totalCalories = 0;
+        float totalPercentage = 0;
+        for(productManager product: food)    {
+            totalCalories += product.getTotalCalories();
+
+            float percentage = (float) (product.getTotalCalories()/calorieGoal);
+            totalPercentage += percentage;
+            String name = product.getName();
+            entries.add(new PieEntry(percentage, name));
+        }
+        Log.d("totalp", String.valueOf(totalPercentage));
+        if(totalPercentage < 1f){
+            entries.add(new PieEntry(1f-totalPercentage, ""));
+        }
+        pieChart.setCenterText(String.format("%.0f", totalPercentage*100)+ "%");
+
+        ArrayList<Integer> colors = new ArrayList<>();
+        float calorie = (float) totalCalories;
+        Log.d("Cals", String.valueOf(calorie));
+        float percentageEaten = (calorie/calorieGoal)*100;
+        if(percentageEaten <= 50){
+            for (int i = 0; i < food.size(); i++) {
+                colors.add(Color.RED);
+            }
+        }
+        if(percentageEaten > 50 && percentageEaten <= 80){
+            for (int i = 0; i < food.size(); i++) {
+                colors.add(Color.rgb(255, 106, 0));
+            }
+        }
+        if(percentageEaten > 80){
+            for (int i = 0; i < food.size(); i++) {
+                colors.add(Color.GREEN);
+            }
+        }
+
+        colors.add(Color.rgb(238, 234, 249));
+
+
+        PieDataSet dataSet = new PieDataSet(entries, "Categories");
+        dataSet.setColors(colors);
+
+        PieData data = new PieData(dataSet);
+        data.setDrawValues(true);
+        data.setValueFormatter(new PercentFormatter(pieChart));
+        data.setValueTextSize(12f);
+        data.setValueTextColor(Color.rgb(11, 4, 82));
+
+        Legend L = pieChart.getLegend();
+        L.setEnabled(false);
+
+        pieChart.setData(data);
+        pieChart.invalidate();
     }
 
     public void loadSpinner(){
